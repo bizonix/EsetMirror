80a855885492901740f3b7c14c4adc6b4dabd7a5
BiZoNiX <bizonix@me.com>
2013-05-20 21:48:39 +0300

    init

---
+++ config.example.php
@@ -0,0 +1,13 @@
+<?php
+define('HOME', '/home/eset');
+define('DOWNLOAD_DIRECTORY', HOME . '/eset');
+define('OLD_UPDATE_DIRECTORY', HOME . '/update_old');
+define('unrar', '/usr/local/bin/unrar');
+
+define('DropBox', '12345678'); 
+define('APPLICATION_KEY', 'xxx');
+define('APPLICATION_SECRET', 'xxx');
+
+define('TOKEN', 'xxx');
+define('TOKEN_SECRET', 'xxx');
+?>
